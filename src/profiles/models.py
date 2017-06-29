3f8657132ed34f9a1f806ceaab36099eea0ab87b
josephturi <josephturi@gmail.com>
2017-06-29 15:25:47 +0100

    static and media added

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,3 +1,13 @@
+from __future__ import unicode_literals
+
 from django.db import models
 
 # Create your models here.
+class profile(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField(default='default text')
+    location = models.CharField(max_length=120, null=True, blank=True)
+    job = models.CharField(max_length=120, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
