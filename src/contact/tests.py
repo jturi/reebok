fcc9aff195bf8fd5f5cb151d5f6907743a87c2bc
josephturi <josephturi@gmail.com>
2017-06-30 11:24:11 +0100

    django-allauth authentication added

--- src/contact/tests.py
+++ src/contact/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+for i in range(1, 150):
+    for j in range(1, 150):
+        if i / j > 0.447:
+            if i / j < 0.448:
+                div = i / j
+                fstring = f'{div:.4f}'
+                print("Results: %d / %d = %s" %(i, j, fstring))
+
--- src/reebok/urls.py
+++ src/reebok/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from src.profiles import views as profile_views
 from src.contact import views as contact_views
@@ -27,8 +27,11 @@ urlpatterns = [
     url(r'^$', profile_views.home, name='home'),
     url(r'^about/$', profile_views.about, name='about'),
     url(r'^contact/$', contact_views.contact, name='contact'),
+    url(r'^accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
