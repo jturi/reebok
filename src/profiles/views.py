a592ef3fe019eb2492a006aac17fec46e7a92846
josephturi <josephturi@gmail.com>
2017-07-01 10:54:34 +0100

    checkout added

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
@@ -12,4 +12,11 @@ def home(request):
 def about(request):
     template = 'about.html'
     context = {}
+    return render(request, template, context)
+
+@login_required
+def userProfile(request):
+    user = request.user
+    context = {'user': user}
+    template = 'profile.html'
     return render(request, template, context)
